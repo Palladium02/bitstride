314feba625f49e7423475e6e65841efeeab836b5
Palladium02 <seanmeyer544@gmail.com>
2025-06-17 13:29:24 +0200

    in between save

---
+++ balancer/src/config.rs
@@ -0,0 +1,32 @@
+use anyhow::Result;
+use serde::Deserialize;
+use std::{fs::File, io::Read, path::Path};
+use thiserror::Error;
+use toml;
+
+#[derive(Debug, Error)]
+pub enum ConfigError {
+    #[error("No such file")]
+    Enoent,
+    #[error("Failed to read file")]
+    Io,
+    #[error("Failed to parse config")]
+    Parse,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config;
+
+impl Config {
+    pub fn from_path<P>(path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let mut buffer = String::new();
+        File::open(path)
+            .map_err(|_| ConfigError::Enoent)?
+            .read_to_string(&mut buffer)
+            .map_err(|_| ConfigError::Io)?;
+        Ok(toml::from_str::<Self>(&buffer).map_err(|_| ConfigError::Parse)?)
+    }
+}
--- balancer/src/main.rs
+++ balancer/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use anyhow::Result;
+use config::Config;
+use std::env::args;
+
+fn main() -> Result<()> {
+    match args().skip(1).nth(0) {
+        Some(config_path) => {
+            let _ = Config::from_path(config_path)?;
+        }
+        None => {
+            eprint!(
+                "Missing argument for path to config\nUsage: bitstride path/to/stride.toml or cargo run -p balancer -- path/to/stride.toml (when running from source)\n"
+            )
+        }
+    }
+
+    Ok(())
 }
