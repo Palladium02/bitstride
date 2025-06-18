382ffa6d54d7cb7248231821c86b3782278bca5f
Palladium02 <seanmeyer544@gmail.com>
2025-06-18 17:57:22 +0200

    added stub handler for report_health

--- balancer/src/health.rs
+++ balancer/src/health.rs
@@ -12,6 +12,8 @@ pub struct HealthService;
 #[tonic::async_trait]
 impl Health for HealthService {
     async fn report_health(&self, request: Request<HealthData>) -> Result<Response<Empty>, Status> {
-        todo!()
+        println!("Got request {:?}", request.into_inner());
+        
+        Ok(Response::new(Empty {}))
     }
 }
